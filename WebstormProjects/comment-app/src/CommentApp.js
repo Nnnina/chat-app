8ce1810ca5cf2e9a4dcf585a76fdfe8d36c360c4
Nnnina <nnninalei@gmail.com>
2018-03-07 12:54:09 -0500

    +localStorage

--- WebstormProjects/comment-app/src/CommentApp.js
+++ WebstormProjects/comment-app/src/CommentApp.js
@@ -6,27 +6,50 @@ class CommentApp extends Component {
     constructor () {
         super()
         this.state = {
-            comments: [
-                {username: '木之本🌸', content: '喜ぶ'}
-            ]
+            comments: []
         }
+    }
+
+    componentWillMount() {
+        this._loadComments()
+    }
+
+    _loadComments() {
+        let comments = localStorage.getItem('comments')
+        if (comments) {
+            comments = JSON.parse(comments);
+            this.setState({ comments })
+        }
+    }
+
+    _saveComments(comments) {
+        localStorage.setItem('comments', JSON.stringify(comments))
     }
 
     handleSubmitComment (comment) {
         if (!comment) return;
         if (!comment.username) return alert('请输入用户名');
         if (!comment.content) return alert('请输入评论内容');
-        this.state.comments.push(comment);
-        this.setState({
-            comments: this.state.comments
-        })
+        const comments = this.state.comments
+        comments.push(comment)
+        this.setState({ comments })
+        this._saveComments(comments)
+    }
+
+    handleDeleteComment (index) {
+        const comments = this.state.comments
+        comments.splice(index, 1)
+        this.setState({ comments })
+        this._saveComments(comments)
     }
 
     render() {
         return (
             <div className='wrapper'>
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList comments={this.state.comments}/>
+                <CommentList
+                    comments={this.state.comments}
+                    onDeleteComment={this.handleDeleteComment.bind(this)}/>
             </div>
         )
     }
